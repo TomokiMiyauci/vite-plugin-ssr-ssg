a5460239380b9f7d22d1e7555d0621b22f6f8c8c
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-03-16 09:55:53 +0900

    :sparkles:  Add auto detect routes from pages file

--- src/react/routes.tsx
+++ src/react/routes.tsx
@@ -1,21 +1,28 @@
-import { toRootAbsolute } from '../utils'
-import { lazy, Suspense } from 'react'
+import { lazy, Suspense, ComponentType } from 'react'
 
-const pages = import.meta.glob(toRootAbsolute('src', 'pages', '*.tsx'))
-const routes = Object.keys(pages).map((path) => {
-  const name = path.match(/\.\/pages\/(.*)\.tsx$/)?.[1]
-  const Page = lazy(pages[path] as any)
-
-  return {
-    name,
-    path: name === 'Index' ? '/' : `/${name?.toLowerCase()}`,
-
-    Component: () => (
-      <Suspense fallback={<></>}>
-        <Page />
-      </Suspense>
+const pages = import.meta.glob('../../src/pages/*.tsx')
+const routes = Object.keys(pages)
+  .filter((page) => /.+\.(tsx?|jsx?)(\?v=[a-z0-9]+)?$/.test(page))
+  .map((path) => {
+    const fileName = path.split('/').slice(-1)[0]
+    const name = fileName.split('.')[0]
+    const Page = lazy(
+      pages[path] as () => Promise<{
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        default: ComponentType<any>
+      }>
     )
-  }
-})
+
+    return {
+      name,
+      path: name === 'Index' ? '/' : `/${name?.toLowerCase()}`,
+
+      Component: () => (
+        <Suspense fallback={<></>}>
+          <Page />
+        </Suspense>
+      )
+    }
+  })
 
 export { routes }
