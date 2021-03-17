c9912aa6b2cdae899894140441233c85b06af213
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-03-17 13:52:23 +0900

    :bug: (react) Change getRoutes interface

--- src/react/routes.tsx
+++ src/react/routes.tsx
@@ -1,27 +1,35 @@
-import { lazy, ComponentType } from 'react'
+import { lazy, ComponentType, LazyExoticComponent } from 'react'
 
-const getRoutes = () => {
-  const pages = import.meta.glob('./pages/*.tsx')
+type GetRoutes = {
+  name: string
+  path: string
+  Component: LazyExoticComponent<ComponentType<any>>
+}
 
-  return Object.keys(pages)
-    .filter((page) => /.+\.(tsx?|jsx?)(\?v=[a-z0-9]+)?$/.test(page))
-    .map((path) => {
-      const fileName = path.split('/').slice(-1)[0]
-      const name = fileName.split('.')[0]
-      const Page = lazy(
-        pages[path] as () => Promise<{
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          default: ComponentType<any>
-        }>
-      )
+const getRoutes = (
+  pages: Record<
+    string,
+    () => Promise<{
+      [key: string]: any
+    }>
+  >
+): GetRoutes[] => {
+  return Object.keys(pages).map((path) => {
+    const fileName = path.split('/').slice(-1)[0]
+    const name = fileName.split('.')[0]
+    const Page = lazy(
+      pages[path] as () => Promise<{
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        default: ComponentType<any>
+      }>
+    )
 
-      return {
-        name,
-        path: name === 'Index' ? '/' : `/${name?.toLowerCase()}`,
-
-        Component: Page
-      }
-    })
+    return {
+      name,
+      path: name === 'Index' ? '/' : `/${name?.toLowerCase()}`,
+      Component: Page
+    }
+  })
 }
 
 export { getRoutes }
