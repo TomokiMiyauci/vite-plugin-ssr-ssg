418b6e92df3d6e786f6d7df27ed1b095ab88bf3d
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-03-17 21:58:17 +0900

    :bug: (react) Import sync

--- src/react/routes.tsx
+++ src/react/routes.tsx
@@ -1,35 +1,29 @@
-import { lazy, ComponentType, LazyExoticComponent } from 'react'
+import { ComponentType } from 'react'
 
 type GetRoutes = {
   name: string
   path: string
-  Component: LazyExoticComponent<ComponentType<any>>
+  Component: ComponentType
 }
 
 const getRoutes = (
   pages: Record<
     string,
-    () => Promise<{
-      [key: string]: any
-    }>
-  >
-): GetRoutes[] => {
-  return Object.keys(pages).map((path) => {
+    {
+      [key: string]: ComponentType
+    }
+  >,
+  key = 'default'
+): GetRoutes[] =>
+  Object.entries(pages).map(([path, fn]) => {
     const fileName = path.split('/').slice(-1)[0]
     const name = fileName.split('.')[0]
-    const Page = lazy(
-      pages[path] as () => Promise<{
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        default: ComponentType<any>
-      }>
-    )
 
     return {
       name,
       path: name === 'Index' ? '/' : `/${name?.toLowerCase()}`,
-      Component: Page
+      Component: fn[key]
     }
   })
-}
 
 export { getRoutes }
