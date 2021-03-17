26587779dd13bebe3acbd250a1535232ea911417
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-03-17 12:18:19 +0900

    :sparkles: (react) Add routes

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,10 +1,13 @@
 import { renderToString } from 'react-dom/server'
 import type { StaticRouterContext } from 'react-router'
 
+const pages = import.meta.glob('./src/pages/*.tsx')
+
 export { getRoutes } from './routes'
 type ServerRenderer = (
   url: string,
   context: StaticRouterContext
 ) => Promise<ReturnType<typeof renderToString>>
 
+export { pages }
 export type { ServerRenderer }
