ccc0795697c3865b47e69aeebc19e2e32fa03c7a
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-03-19 22:05:39 +0900

    :bug:  Remove unused

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,3 @@
-export const isTest =
-  process.env.NODE_ENV === 'test' || !!process.env.VITE_TEST_BUILD
-export const ROOT_PATH = require('app-root-path').path
 export const EXTENSIONS = ['jsx', 'tsx', 'ts', 'js', 'vue'] as const
 export const PIPED_EXTENSIONS = EXTENSIONS.join('|')
 export const NAME = 'vite:ssr-ssg'
